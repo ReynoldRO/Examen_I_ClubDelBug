f6c1ff93df9facbf94b0f5ab6bead0bc6d00f5a1
gipsyAl <giaguirrelo@est.utn.ac.cr>
2025-07-04 16:04:31 -0600

    Se crea la clase ListaResidentes

---
+++ src/MetodosGenericos/MetodosGenericos.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package MetodosGenericos;
+
+
+
+/**
+ *
+ * @author Student
+ */
+public class MetodosGenericos {
+  static Residente ListaResidentes[]; 
+
+ public boolean buscar T (Residente[] ListaResidentes){
+     for (int i=0; i<100; i++){
+     if String.valueOf(id).equals.Empty.getIdCedula {
+     return IdCedula[i]; 
+ }
+  return null;
+}
+     
+     
+     
+public boolean eliminar T(Residente[] ListaResidente){
+    for(int i=0; i<100; i++){
+        if(IdCedula[i]==null) {
+            if(IdCedula[i]==Cedula);
+        }
+    }
+    return IdCedula[i];
+    
+}
+    return null;
+}
+        
+        
+public boolean Agregar T(Residente[] ListaResidente){
+     for (int i=0; i<100; i++){
+         if(IdCedula[i]==null) {
+             IdCedula[i] = Nombre;
+             return true;
+             
+         }
+     }
+     return false;
+}
